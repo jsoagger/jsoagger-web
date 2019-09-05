05758b9d1bea0262454a1b82ab92e77bd28acb62
yvonjisoa <rmvonji@gmail.com>
2019-09-05 21:01:43 +0200

    integrate maven

---
+++ src/main/java/io/github/jsoagger/core/webui/JSoaggerWebUI.java
@@ -0,0 +1,14 @@
+
+package io.jsoagger.core.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSoaggerWebUI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JSoaggerWebUI.class, args);
+    }
+
+}
